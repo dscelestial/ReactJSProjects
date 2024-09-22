57b789d238e4ada6e9b2f231b2f5098c8c01debf
dscelestial <celestial.danielstephen@gmail.com>
2024-09-22 22:19:27 +0800

    board complete

--- TicTacToe/src/Board/Board.jsx
+++ TicTacToe/src/Board/Board.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Board = () => {
   return (
     <>
-        <div className='board'>This is a board</div>
+        <div className='board'>
+          <div className='board-row'>
+            <button className='square'>X</button>
+            <button className='square'>X</button>
+            <button className='square'>X</button>
+          </div>
+          <div className='board-row'>
+            <button className='square'>X</button>
+            <button className='square'>X</button>
+            <button className='square'>X</button>
+          </div>
+          <div className='board-row'>
+            <button className='square'>X</button>
+            <button className='square'>X</button>
+            <button className='square'>X</button>
+          </div>
+        </div>
     </>
   )
 }
