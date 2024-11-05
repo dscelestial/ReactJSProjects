f3e3dd4e70957b6d5cd8963ad97ee6a0d678e36c
Daniel Celestial <celestial.danielstephen@gmail.com>
2024-11-05 11:19:40 +0800

    Game board

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,22 @@
 function App() {
   return (
-    <p>TESTING</p>
+    <main>
+      <div id="game-container">
+
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+
+        GAME BOARD
+      </div>
+    </main>
   )
 }
 
