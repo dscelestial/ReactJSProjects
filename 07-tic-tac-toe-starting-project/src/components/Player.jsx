dfb778a29e284679d989f10284c2b658ba14f205
Daniel Celestial <celestial.danielstephen@gmail.com>
2024-11-06 17:57:49 +0800

    GameBoard component

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
 
-const Player = ({ name, symbol }) => {
-    const [ isEditing, setIsEditing ] = useState(false);
+const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
 
-    const onClick = function(){
-        setIsEditing(!isEditing);
+    const onClick = () => {
+        setIsEditing(editing => !editing);
     }
 
-  return (
-    <li>
-        <span className="player">
-            {isEditing ? (<input type="text" required value={name}></input>) : (<span className="player-name">{name}</span>)}
-            <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={onClick}>{isEditing ? "SAVE" : "EDIT"}</button>
-    </li>
-  )
+    const onChange = (e) => {
+        setPlayerName(e.target.value);
+    }
+
+    return (
+      <li>
+          <span className="player">
+              {isEditing ? (<input type="text" required value={playerName} onChange={onChange}></input>) : (<span className="player-name">{playerName}</span>)}
+              <span className="player-symbol">{symbol}</span>
+          </span>
+          <button onClick={onClick}>{isEditing ? "SAVE" : "EDIT"}</button>
+      </li>
+    )
 }
 
 export default Player
